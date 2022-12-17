99a68606c095c0dafa377a8c4e09430eedc5e170
Mikee1353 <murat_tunc07@windowslive.com>
2022-12-17 03:09:56 +0300

    a
    a

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 MoveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation= true;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();   
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        MoveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(MoveDirection.normalized * moveSpeed * 10f, ForceMode.Acceleration);
+    }
+}
